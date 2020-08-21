4e8e8942ca612faa2d29bc0813152a6acbabaaed
Bobo <dongkaige@gmail.com>
2020-08-21 11:44:45 +0800

    refa:user login handler

--- video-server/api/handlers.go
+++ video-server/api/handlers.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/go-video-website/api/dbops"
@@ -37,6 +37,40 @@ func CreateUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 }
 
 func Login(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	uname := p.ByName("user_name")
-	io.WriteString(w, uname)
+	res, _ := ioutil.ReadAll(r.Body)
+	log.Printf("%s", res)
+	ubody := &defs.UserCredential{}
+	if err := json.Unmarshal(res, ubody); err != nil {
+		log.Printf("%s", err)
+		// io.WriteString(w, "wrong")
+		sendErrorResponse(w, defs.ErrorRequestBodyParseFailed)
+		return
+	}
+
+	// Validate the request body
+	uname := p.ByName("username")
+	log.Printf("Login url name: %s", uname)
+	log.Printf("Login body name: %s", ubody.Username)
+	if uname != ubody.Username {
+		sendErrorResponse(w, defs.ErrorNotAuthUser)
+		return
+	}
+
+	log.Printf("%s", ubody.Username)
+	pwd, err := dbops.GetUserCredential(ubody.Username)
+	log.Printf("Login pwd: %s", pwd)
+	if err != nil || len(pwd) == 0 || pwd != ubody.Pwd {
+		sendErrorResponse(w, defs.ErrorNotAuthUser)
+		return
+	}
+
+	id := session.GenerateNewSessionId(ubody.Username)
+	si := &defs.SignedIn{Success: true, SessionId: id}
+	if resp, err := json.Marshal(si); err != nil {
+		sendErrorResponse(w, defs.ErrorInternalFaults)
+	} else {
+		sendNormalResponse(w, string(resp), 200)
+	}
+
+	//io.WriteString(w, "signed in")
 }
